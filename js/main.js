0bb0986a629da5c6587b83a2f2771d09549dd01f
catanielena <catani.elena1@gmail.com>
2021-09-22 11:28:34 +0200

    Add: milestone--2

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ const app = new Vue ({
             {
                 name: 'Fabio',
                 avatar: '_2',
-                visible: true,
+                visible: false,
                 messages: [{
                     date: '20/03/2020 16:30:00',
                     message: 'Ciao come stai?',
@@ -48,7 +48,7 @@ const app = new Vue ({
             {
                 name: 'Samuele',
                 avatar: '_3',
-                visible: true,
+                visible: false,
                 messages: [{
                     date: '28/03/2020 10:10:40',
                     message: 'La Marianna va in campagna',
@@ -69,7 +69,7 @@ const app = new Vue ({
             {
                 name: 'Luisa',
                 avatar: '_4',
-                visible: true,
+                visible: false,
                 messages: [{
                     date: '10/01/2020 15:30:55',
                     message: 'Lo sai che ha aperto una nuova pizzeria?',
@@ -82,9 +82,24 @@ const app = new Vue ({
                     }
                 ],
             },
-        ]
+        ],
+        activeChat :[]
     },
     methods: {
-
+        changeActiveChat: function(i) {
+            this.activeChat= {};
+            this.contacts.forEach((e) => e.visible = false);
+            this.activeChat = this.contacts.filter((e, index) => {
+                if(index==i) {
+                    e.visible = true;
+                    return {
+                        ...e
+                    }
+                }
+            });
+        }
+    },
+    created: function() {
+        this.activeChat.push(this.contacts[0])
     }
 });
